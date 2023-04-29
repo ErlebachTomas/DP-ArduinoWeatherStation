664b94fda5874975f5af6165dc73a90f01395853
Tomáš Erlebach <t.erleb@seznam.cz>
2023-04-29 22:03:33 +0200

    BMP280

--- DP-ArduinoWeatherStation.ino
+++ DP-ArduinoWeatherStation.ino
@@ -12,30 +12,45 @@ DHT sensorDHT(DHTpin, DHTtype);
 //== DISPLAY ==
 #include <U8glib.h>
 U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_NONE);
-
+// == BMP280 ==
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#define BMP280_ADRESS (0x76)
+Adafruit_BMP280 bmp;
 
 void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);  // LED
   bluetooth.begin(9600); // Bluetooth init 
   Serial.begin(9600);
-
   sensorDHT.begin();
-  display.setFont(u8g_font_unifont);   
 
+  display.setFont(u8g_font_unifont);   
+  
+  if (!bmp.begin(BMP280_ADRESS)) {   // osetreni sbernice 
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
 }
 
 void loop() {
 
   float humidity = sensorDHT.readHumidity();
-  Serial.print("DHT: ");
-  Serial.println(humidity);
+  float tepBMP= bmp.readTemperature();
 
-  display.firstPage();
- do {
+
+    display.firstPage();
+  do {
    display.setPrintPos(0, 10);
-   display.print("DHT: ");
+   display.print("Vlhkost: ");
    display.print(humidity);
+   display.print(" %");
+   display.setPrintPos(0, 30);
+   display.print("Vzduch: ");
+   display.print(tepBMP);
+   display.print(" C");   
+
    } while (display.nextPage());
   
 
