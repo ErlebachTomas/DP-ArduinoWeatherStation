32e7005295e2cb585d23fc1098051722ff55138f
Tomáš Erlebach <t.erleb@seznam.cz>
2023-04-29 22:21:39 +0200

    D18B20

--- DP-ArduinoWeatherStation.ino
+++ DP-ArduinoWeatherStation.ino
@@ -18,6 +18,13 @@ U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_NONE);
 #include <Adafruit_BMP280.h>
 #define BMP280_ADRESS (0x76)
 Adafruit_BMP280 bmp;
+// == D18B20 ==
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define D18B20pin 2
+OneWire oneWire(D18B20pin);
+DallasTemperature snowSensor(&oneWire); 
+
 
 void setup() {
 
@@ -25,32 +32,42 @@ void setup() {
   bluetooth.begin(9600); // Bluetooth init 
   Serial.begin(9600);
   sensorDHT.begin();
-
-  display.setFont(u8g_font_unifont);   
-  
+  snowSensor.begin();
+      
   if (!bmp.begin(BMP280_ADRESS)) {   // osetreni sbernice 
     Serial.println("Could not find a valid BMP280 sensor, check wiring!");
     while (1);
   }
+
+  display.setFont(u8g_font_unifont); 
 }
 
 void loop() {
 
   float humidity = sensorDHT.readHumidity();
-  float tepBMP= bmp.readTemperature();
+  float tepBMP = bmp.readTemperature();
+  snowSensor.requestTemperatures();  
+  float tepSnow = snowSensor.getTempCByIndex(0); // prvni čidlo
+  float pressure = (bmp.readPressure()/100.00) + 32; // korekce měření v hPa
 
-
-    display.firstPage();
+  display.firstPage();
   do {
    display.setPrintPos(0, 10);
+   display.print("Vzduch: ");
+   display.print(tepBMP);
+   display.print(" C"); 
+   display.setPrintPos(0, 30);
+   display.print("Sníh: ");
+   display.print(tepSnow);
+   display.print(" C");  
+   display.setPrintPos(0, 45);
    display.print("Vlhkost: ");
    display.print(humidity);
    display.print(" %");
-   display.setPrintPos(0, 30);
-   display.print("Vzduch: ");
-   display.print(tepBMP);
-   display.print(" C");   
-
+   display.setPrintPos(0, 60);
+   display.print("Tlak: ");
+   display.print(pressure);
+   display.print(" hPa");
    } while (display.nextPage());
   
 
