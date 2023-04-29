975ca9e07b8c4437d961f6df6999e3a96afca15e
Tomáš Erlebach <t.erleb@seznam.cz>
2023-04-29 21:33:47 +0200

    Humidity sensor

--- DP-ArduinoWeatherStation.ino
+++ DP-ArduinoWeatherStation.ino
@@ -1,20 +1,44 @@
 #include <SoftwareSerial.h>
-#include <SoftwareSerial.h>
 
-// Bluetooth
-#define RX 11
+// == BLUETOOTH modul ==
+#define RX 11 
 #define TX 10
 SoftwareSerial bluetooth(TX, RX);
+//== VLHKOMER  ==
+#include <DHT.h>
+#define DHTpin 5 
+#define DHTtype DHT11  
+DHT sensorDHT(DHTpin, DHTtype);
+//== DISPLAY ==
+#include <U8glib.h>
+U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_NONE);
+
 
 void setup() {
-  
+
   pinMode(LED_BUILTIN, OUTPUT);  // LED
   bluetooth.begin(9600); // Bluetooth init 
   Serial.begin(9600);
 
+  sensorDHT.begin();
+  display.setFont(u8g_font_unifont);   
+
 }
 
 void loop() {
+
+  float humidity = sensorDHT.readHumidity();
+  Serial.print("DHT: ");
+  Serial.println(humidity);
+
+  display.firstPage();
+ do {
+   display.setPrintPos(0, 10);
+   display.print("DHT: ");
+   display.print(humidity);
+   } while (display.nextPage());
+  
+
   if (bluetooth.available() > 0) {    
     byte receivedData = bluetooth.read();  
     
