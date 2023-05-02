f62e2124a363d66078fdf7d110e517c91531b1d4
Tomáš Erlebach <t.erleb@seznam.cz>
2023-05-02 13:03:52 +0200

    Arduino simulator

---
+++ VirtualArduinoSimulator/Localization.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Resources;
+
+namespace VirtualArduinoSimulator
+{
+    /// <summary>
+    /// Třída pro lokalizaci konzole
+    /// </summary>
+    public static class Localization
+    {
+        /// <summary>
+        /// ResourceManager pro získání řetězců z Resource Files.
+        /// </summary>
+        private static readonly ResourceManager ResourceManager = new ResourceManager(typeof(Resources.Strings));
+        
+        /// <summary>
+        /// Vrací lokalizovaný řetězec 
+        /// </summary>
+        /// <param name="key">Klíč</param>
+        /// <param name="args">Proměné pro vložení do textu</param>       
+        public static string getString(string key, params object[] args)
+        {
+            string format = ResourceManager.GetString(key);
+            return string.Format(format, args);
+        }
+        /// <summary>
+        /// lokalizovaný WriteLine jako extension metoda stringu
+        /// </summary>
+        public static void printLocalizedLine(this string key, params object[] args)
+        {
+            Console.WriteLine(getString(key, args));
+        }
+        /// <summary>
+        /// lokalizovaný print
+        /// </summary>
+        public static void printLocalized(this string key, params object[] args)
+        {
+            Console.Write(getString(key, args));
+        }
+    }
+}
---
+++ VirtualArduinoSimulator/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using Newtonsoft.Json.Linq;
+//using loc = VirtualArduinoSimulator.Localization; // alias
+
+namespace VirtualArduinoSimulator
+{
+    /// <summary>
+    /// Program pro simulaci Bluetooth komunikace s meteostanicí
+    /// </summary>
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            "version".printLocalizedLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());           
+            "info".printLocalizedLine();
+            Console.WriteLine();
+
+            IsBluetoothEnabled();
+
+            var serviceClass = BluetoothService.SerialPort;
+            var listener = new BluetoothListener(serviceClass);
+            listener.Start();
+
+            var radio = BluetoothRadio.Default.LocalAddress;
+            
+            "listening".printLocalizedLine(radio.ToString("C"), serviceClass);
+
+            while (true)
+            {
+                var client = listener.AcceptBluetoothClient();
+                _ = Task.Run(() => HandleClient(client));
+            }
+
+        }
+        /// <summary>
+        /// Obsluha spojení a odesílání zpráv
+        /// </summary>
+        private static void HandleClient(BluetoothClient client)
+        {
+            "connected".printLocalizedLine();
+            
+            using (client)
+            {
+                var stream = client.GetStream();
+                var reader = new StreamReader(stream, Encoding.ASCII);
+                var writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+
+                while (client.Connected)
+                {
+                    int receivedValue = reader.Read();
+
+                    if (receivedValue == -1)
+                    {
+                        break; // pro disconnected
+
+                    }
+                    switch (receivedValue) // musí odpovídat kódu na desce!!
+                    {
+                        case '0':
+                            "LED Off".printLocalizedLine();
+                            break;
+                        case '1':
+                            "LED ON".printLocalizedLine();
+                             break;
+                        case '2':
+                            "sended via Bluetooth".printLocalizedLine();
+                            var jsonString = generateRandomJSONdataObject();
+                            writer.Write(jsonString);
+                            Console.WriteLine(jsonString);
+                            break;
+                        case 13:
+                            break;
+                        case 10:
+                            break; // ignoruj konce řádku
+                        default:
+                            "Unknow command".printLocalizedLine(receivedValue);
+                            break;
+                    }
+                }
+            }
+            "disconnected".printLocalizedLine();
+        }
+
+        /// <summary>
+        /// vygeneruje vzorový JSON objekt (musí odpovídat kódu na desce)
+        /// </summary>
+        private static string generateRandomJSONdataObject()
+        {
+            var random = new Random();
+            double airTemperature = random.Next(-15, 11) + random.NextDouble();
+            double snowTemperature = random.Next(-15, 11) + random.NextDouble();
+            double humidity = random.Next(20, 91) + random.NextDouble();
+
+            var jsonDoc = new JObject
+            {
+                ["airTemperature"] = Math.Round(airTemperature, 1),
+                ["snowTemperature"] = Math.Round(snowTemperature, 1),
+                ["humidity"] = Math.Round(humidity, 1)
+            };
+
+            return jsonDoc.ToString();
+           
+        }
+        /// <summary>
+        /// kontroluje, zda je Bluetooth zapnuto
+        /// </summary>
+        public static bool IsBluetoothEnabled()
+        {
+            var radio = BluetoothRadio.Default;
+            if (radio == null)
+            {
+                "No Bluetooth adapter found".printLocalizedLine();
+                return false;
+            }
+
+            if (radio.Mode == RadioMode.PowerOff)
+            {
+                "Bluetooth is turned off".printLocalizedLine();
+                return false;
+            }
+
+            "Bluetooth is enabled".printLocalizedLine();
+            return true;
+        }
+
+
+    }
+
+  
+
+}
